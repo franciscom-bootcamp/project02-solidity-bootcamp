98797bff2c0f7ad55e72262ee354eadefb416dc3
franciscom-bootcamp <encode@mail.fmf.rocks>
2022-09-29 23:53:15 +0100

    Added Ballot Deployment script to Deploy on Goerli and import custom PrivateKey

---
+++ scripts/DeploymentGoerli.ts
@@ -0,0 +1,49 @@
+import { ethers } from "ethers";
+import { Ballot__factory } from "../typechain-types";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
+
+function convertStringArrayToBytes32(array: string[]) {
+  const bytes32Array = [];
+  for (let index = 0; index < array.length; index++) {
+    bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
+  }
+  return bytes32Array;
+}
+
+async function main() {
+  const provider = ethers.getDefaultProvider("goerli");
+
+  const signer = new ethers.Wallet(process.env.PRIVATE_KEY_1, provider);
+
+  const balanceBN = await signer.getBalance();
+  const balance = Number(ethers.utils.formatEther(balanceBN));
+  console.log({
+    "Address1": signer.address,
+    "balance": balance
+  });
+
+  const ballotFactory = new Ballot__factory(signer);
+  
+  console.log("Deploying Ballot contract");
+  console.log("Proposals: ");
+  PROPOSALS.forEach((element, index) => {
+    console.log(`Proposal N. ${index + 1}: ${element}`);
+  });
+  
+  const ballotContract = await ballotFactory.deploy(
+      convertStringArrayToBytes32(PROPOSALS)
+    );
+  
+  await ballotContract.deployed();
+
+  console.log(`Ballot deployed to ${ballotContract.address} on Goerli`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
